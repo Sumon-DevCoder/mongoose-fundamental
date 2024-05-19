dddabd743518c58d051ee0945a36a9f9ce99bbf3
SumonRahman24 <mustafizurrahmansumon24@gmail.com>
2024-05-19 23:07:46 +0600

    some features added

---
+++ __notes.js
@@ -0,0 +1,7 @@
+// install package .json file --> npm init -y and paste this code: "build": "tsc",
+
+// installation setup --> npm i nodemon express mongoose dotenv cors
+//                    --> npm i -D typescript
+
+// ts folder structure setup --> src -> ( server.ts, app.ts )
+//
